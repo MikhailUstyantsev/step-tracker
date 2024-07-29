49622129d3eb16e4ffb80e9a4ca6048f91adcc85
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2024-07-29 23:30:19 +0500

    created xcode project

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Mikhail Ustyantsev on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
